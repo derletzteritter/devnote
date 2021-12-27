adc430542385fb997e9a9a6b6212e247a364cd28
itschip <onlygamernerd@gmail.com>
2021-12-27 16:48:34 +0100

    feat(wnd): markdown/editor wnd

--- internal/wnd/create_wnd.go
+++ internal/wnd/create_wnd.go
@@ -1,6 +1,7 @@
 package wnd
 
 import (
+	"fmt"
 	"github.com/AllenDang/giu"
 	"github.com/itschip/dev-note/internal/files"
 )
@@ -11,19 +12,73 @@ const (
 
 var (
 	searchValue string
+	showFileDialog bool
+	newFileName string
+	fileContent string
+	showFileEditor bool
+	editor *giu.CodeEditorWidget
 )
 
+func onShowFileDialog() {
+	showFileDialog = true
+}
+
+func onHideFileDialog()  {
+	showFileDialog = false
+}
+
+func onShowFileEditor() {
+	showFileEditor = true
+}
+
+func onHideFileEditor() {
+	showFileEditor = false
+}
+
 func wndLoop() {
+
 	giu.SingleWindow().Layout(
 		giu.Label("Search for files"),
 		giu.Spacing(),
 		giu.Row(
 			giu.InputText(&searchValue),
-			giu.Button("New file")),
-		giu.Label("All files"), giu.Table().Rows(files.BuildFilesLayout()...))
+			giu.Button("New file").OnClick(onShowFileDialog)),
+		giu.Label("All files"), giu.Table().Rows(files.BuildFilesLayout(func(file string) {
+			fileContent = file
+
+			editor = giu.CodeEditor().ShowWhitespaces(true).Text(fileContent).Border(true)
+
+			onShowFileEditor()
+		})...))
+
+	if showFileDialog {
+		giu.Window("New file").IsOpen(&showFileDialog).Size(300, 300).
+			Layout(
+				giu.Label("Enter a file name"),
+				giu.InputText(&newFileName),
+				giu.Button("Create file").OnClick(func() {
+					files.CreateNewFile(newFileName)
+					onHideFileDialog()
+				}),
+				giu.Button("Cancel").OnClick(onHideFileDialog))
+	}
+
+	if showFileEditor {
+		if editor.IsTextChanged() {
+			fileContent = editor.GetText()
+			fmt.Println(fileContent)
+		}
+
+		giu.Window("Editor").IsOpen(&showFileEditor).Size(500, 500).
+			Layout(
+				giu.Markdown(&fileContent),
+				editor,
+				giu.Button("Close").OnClick(onHideFileEditor))
+	}
 }
 
 func MasterWindow() {
+
 	wnd := giu.NewMasterWindow(windowTitle, 1000, 700, giu.MasterWindowFlagsNotResizable)
 
 	wnd.Run(wndLoop)
